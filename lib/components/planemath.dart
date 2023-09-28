0777180e7c44b3d78619cc1b218505fd56dc22b8
JoaoRafa19 <joaopedrorafael19@gmail.com>
2023-09-28 11:16:51 -0300

    feat:Project setupt

---
+++ lib/components/planemath.dart
@@ -0,0 +1,5 @@
+import 'package:flame/game.dart';
+
+class Planemath extends FlameGame {
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,16 @@
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:planemath/components/planemath.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Planemath',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  Planemath game = Planemath();
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Planemath',
-            ),
-          ],
-        ),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  runApp(GameWidget(game: kDebugMode ? Planemath() : game));
 }
